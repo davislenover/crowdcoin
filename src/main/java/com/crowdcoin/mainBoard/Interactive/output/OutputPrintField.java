526d53dbda51bd9d6ce3f4a2d4e5780eca49b229
Davis <davislenover02@gmail.com>
2023-07-03 09:34:43 -0400

    F09 - Added OutputPrintField

--- src/main/java/com/crowdcoin/mainBoard/Interactive/output/OutputPrintField.java
+++ src/main/java/com/crowdcoin/mainBoard/Interactive/output/OutputPrintField.java
@@ -8,29 +8,44 @@ import javafx.scene.text.Text;
 
 public class OutputPrintField implements OutputField {
 
+    // Defaults
+    private static int textWrappingWidth = 200;
+
     private StackPane containerPane;
     private Text messageText;
     private int order;
 
+    private InteractivePane parentPane;
+
     public OutputPrintField(String message) {
-        this.order = 0;
+
         this.messageText = new Text(message);
         this.containerPane = new StackPane();
+
+        this.containerPane.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
+        // By default, items are added to the center of the StackPane and the node (as in the JavaFX object like TextField or Text) will be stretched to fit the entire available space
+        this.containerPane.getChildren().add(this.messageText);
+
+        // Set wrapping width
+        this.messageText.setWrappingWidth(textWrappingWidth);
+
+        this.order = 0;
+
     }
 
     @Override
     public void applyPane(GridPane targetPane, int targetRow) {
-
+        targetPane.add(this.containerPane, 0, targetRow);
     }
 
     @Override
     public Pane getPane() {
-        return null;
+        return this.containerPane;
     }
 
     @Override
     public void setInteractivePane(InteractivePane pane) {
-
+        this.parentPane = pane;
     }
 
     @Override
@@ -45,6 +60,6 @@ public class OutputPrintField implements OutputField {
 
     @Override
     public void setValue(String value) {
-
+        this.messageText.setText(value);
     }
 }
