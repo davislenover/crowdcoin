d73c9dd8ca4ea15c8759571105b847f447855c84
Davis <davislenover02@gmail.com>
2023-07-31 10:37:39 -0400

    F14 - Added average ranges to Grade enums, GradeTools is now non-static to reduce amount of queries to database

--- src/main/java/com/crowdcoin/mainBoard/grade/Grade.java
+++ src/main/java/com/crowdcoin/mainBoard/grade/Grade.java
@@ -4,14 +4,20 @@ package com.crowdcoin.mainBoard.grade;
  * Grading Enums used for grading coins
  */
 public enum Grade {
-    Pass(1),Exceed(2),NA(0);
+    Pass(2,2,2.999),Exceed(3,3,3.999),Fail(1,1,1.999);
     private int gradeCode;
+    private double gradeLowRange;
+    private double gradeHighRange;
 
     /**
      * Instantiates a Grade Enum with a Grade code
      * @param gradeCode an Integer value used to compare other Grade Codes. The higher the grade code, the more pretentious the Grade is
+     * @param lowRange the lower bound used to check if a value is in a grade range
+     * @param highRange the upper bound used to check if a value is in a grade range
      */
-    Grade(int gradeCode) {
+    Grade(int gradeCode, double lowRange, double highRange) {
+        this.gradeLowRange = lowRange;
+        this.gradeHighRange = highRange;
         this.gradeCode = gradeCode;
     }
 
@@ -21,5 +27,8 @@ public enum Grade {
      */
     public int getGradeCode() {
         return this.gradeCode;
+    }
+    public boolean isInRange(double value) {
+        return (value >= this.gradeLowRange && value <= this.gradeHighRange);
     }
 }
--- src/main/java/com/crowdcoin/mainBoard/grade/GradeTools.java
+++ src/main/java/com/crowdcoin/mainBoard/grade/GradeTools.java
@@ -2,20 +2,59 @@ package com.crowdcoin.mainBoard.grade;
 
 import com.crowdcoin.networking.sqlcom.data.SQLTable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GradeTools {
 
-    private static int coinIDColumnIndex = 0;
+    private static int gradeColumnStartIndex = 2;
 
-    public static int getGradeAverage(String coinID, SQLTable sqlTable) {
+    private List<Integer> gradeList;
 
-        try {
-            List<List<Object>> row = sqlTable.getSpecificRows(0,coinID,1,0,sqlTable.getNumberOfColumns());
-        } catch (Exception e) {
+    public GradeTools(String coinID, SQLTable table) {
+        this.gradeList = getGradeList(coinID,table);
+    }
 
+    public double getGradeAverage() {
+        int sum = 0;
+        for (Integer grade : this.gradeList) {
+            sum+=grade;
         }
+        return (sum/this.gradeList.size());
+    }
 
+    public Grade getGrade() {
+
+        double average = getGradeAverage();
+        for (Grade grade : Grade.values()) {
+            if (grade.isInRange(average)) {
+                return grade;
+            }
+        }
+        return Grade.Fail;
+    }
+
+    public boolean hasEveryoneGradedID() {
+        for (Integer grade : this.gradeList) {
+            if (grade.intValue() == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> getGradeList(String coinID, SQLTable sqlTable) {
+        try {
+            List<Object> row = sqlTable.getRawSpecificRows(0,coinID,1,0,sqlTable.getNumberOfColumns()).get(0);
+            List<Integer> gradeList = new ArrayList<>();
+            for (int index = gradeColumnStartIndex; index < row.size(); index++) {
+                gradeList.add(Integer.valueOf(row.get(index).toString()));
+            }
+            return gradeList;
+        } catch (Exception e) {
+            // TODO Error handling
+        }
+        return null;
     }
 
 }
