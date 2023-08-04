dc8d5ddbb95295961906a30be9ad5a740b7be066
Davis <davislenover02@gmail.com>
2023-08-04 11:30:58 -0400

    F15 - Added fields layout to AddUserPopWindow

--- src/main/java/com/crowdcoin/mainBoard/window/AddUserPopWindow.java
+++ src/main/java/com/crowdcoin/mainBoard/window/AddUserPopWindow.java
@@ -5,7 +5,11 @@ import com.crowdcoin.mainBoard.Interactive.input.InputField;
 import com.crowdcoin.mainBoard.Interactive.input.InteractiveChoiceBox;
 import com.crowdcoin.mainBoard.Interactive.input.InteractiveTextField;
 import com.crowdcoin.mainBoard.Interactive.input.validation.DoesNotContainValidator;
+import com.crowdcoin.mainBoard.Interactive.input.validation.InputValidator;
 import com.crowdcoin.mainBoard.Interactive.input.validation.LengthValidator;
+import com.crowdcoin.mainBoard.Interactive.output.OutputField;
+import com.crowdcoin.mainBoard.Interactive.submit.InteractiveButton;
+import com.crowdcoin.mainBoard.Interactive.submit.SubmitField;
 import javafx.stage.Stage;
 
 public class AddUserPopWindow extends PopWindow {
@@ -25,8 +29,23 @@ public class AddUserPopWindow extends PopWindow {
         InputField password = new InteractiveTextField("Password","The given starting password for the new account",(event, field, pane1) -> {return;});
         InputField isAdmin = new InteractiveChoiceBox("Is an Admin?","Grants the user elevated privileges",(event, field, pane1) -> {return;},"Yes","No");
 
-        username.addValidator(new LengthValidator(1));
+        InputValidator length = new LengthValidator(1);
+        username.addValidator(length);
         username.addValidator(new DoesNotContainValidator(invalidUsernameSequences));
+        password.addValidator(new LengthValidator(6));
+        isAdmin.addValidator(length);
+        password.setOrder(1);
+        isAdmin.setOrder(2);
+        pane.addInputField(username);
+        pane.addInputField(password);
+        pane.addInputField(isAdmin);
+
+        SubmitField addUserBtn = new InteractiveButton("Add User",(event, button, pane1) -> {return;});
+        SubmitField cancelBtn = new InteractiveButton("Cancel",(event, button, pane1) -> {return;});
+        cancelBtn.setOrder(1);
+
+        pane.addSubmitField(addUserBtn);
+        pane.addSubmitField(cancelBtn);
 
 
     }
