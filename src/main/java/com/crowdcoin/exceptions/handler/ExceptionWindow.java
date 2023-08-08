8e459e2e2ace942a4b7170fe1311aa3e602aea94
Davis <davislenover02@gmail.com>
2023-08-08 12:45:08 -0400

    F16 - Added basic ExceptionWindow class

--- src/main/java/com/crowdcoin/exceptions/handler/ExceptionWindow.java
+++ src/main/java/com/crowdcoin/exceptions/handler/ExceptionWindow.java
@@ -3,21 +3,26 @@ package com.crowdcoin.exceptions.handler;
 import com.crowdcoin.mainBoard.Interactive.InteractiveWindowPane;
 import com.crowdcoin.mainBoard.Interactive.output.OutputField;
 import com.crowdcoin.mainBoard.Interactive.output.OutputPrintField;
+import com.crowdcoin.mainBoard.Interactive.submit.InteractiveButton;
+import com.crowdcoin.mainBoard.Interactive.submit.SubmitField;
 import com.crowdcoin.mainBoard.window.PopWindow;
 import javafx.stage.Stage;
 
-public class ExceptionWindow extends PopWindow  {
+public class ExceptionWindow extends PopWindow {
 
     private String generalMsg = "An unexpected error has occurred";
     private String excMsgPrefix = "Exception Message: ";
     private String stkTracePrefix = "Exception Stack Trace (Technical jargon): ";
+    private String action = "Please report this error to an administrator and restart the program";
     private Throwable exception;
+
     public ExceptionWindow(String windowName, Throwable exception) {
         super(windowName);
         this.exception = exception;
     }
+
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
 
         InteractiveWindowPane pane = super.getWindowPane();
 
@@ -29,13 +34,18 @@ public class ExceptionWindow extends PopWindow  {
         OutputField stkTrace = new OutputPrintField(this.stkTracePrefix + this.stackTraceToString(this.exception));
         stkTrace.setOrder(2);
 
+        OutputField actionOut = new OutputPrintField(this.action);
+        actionOut.setOrder(3);
+
         pane.addOutputField(generalMsg);
         pane.addOutputField(excMsg);
         pane.addOutputField(stkTrace);
+        pane.addOutputField(actionOut);
 
+        SubmitField okBtn = new InteractiveButton("Ok",(event, button, pane1) -> {super.closeWindow();});
+        pane.addSubmitField(okBtn);
 
-
-
+        super.start(stage);
 
     }
 
