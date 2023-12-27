8095e953d4d6513ff11f742cfbfebf189a59325f
Davis <davislenover02@gmail.com>
2023-12-27 15:42:39 -0500

    Revised incrementing/decrementing solution for QueryResultReader. Added all other methods that are needed to work alongside SQLTableReader

--- crowdcoin/src/main/java/com/crowdcoin/newwork/QueryResultReader.java
+++ crowdcoin/src/main/java/com/crowdcoin/newwork/QueryResultReader.java
@@ -22,7 +22,7 @@ public class QueryResultReader implements Iterator<List<Tuple>> {
         }
 
         this.queryResult = queryResult;
-        this.currentStartIndex = 0;
+        this.currentStartIndex = -1;
         this.numberOfRowsPerSet = numberOfRowsPerSet;
 
         this.tupleCount = this.queryResult.getTuplesCount();
@@ -37,7 +37,7 @@ public class QueryResultReader implements Iterator<List<Tuple>> {
      * @param startingIndex the given Tuple starting index (inclusive)
      * @param endingIndex the given Tuple ending index (inclusive)
      * @return an unmodifiable list of {@link Tuple}
-     * @throws InvalidRangeException
+     * @throws InvalidRangeException if the startingIndex is greater than endingIndex. If startingIndex is greater than the largest {@link Tuple} index from {@link QueryResult}
      */
     public List<Tuple> getRows(int startingIndex, int endingIndex) throws InvalidRangeException {
         if (startingIndex > endingIndex) {
@@ -45,8 +45,9 @@ public class QueryResultReader implements Iterator<List<Tuple>> {
         }
 
         List<Tuple> tuples = new ArrayList<>();
-        // TODO, this will fail if starting index is larger than that of the actual result size
-        queryResult.setPosition(startingIndex);
+        if (!queryResult.setPosition(startingIndex)) {
+            throw new InvalidRangeException(String.valueOf(startingIndex),String.valueOf(endingIndex));
+        }
 
         int size = endingIndex - startingIndex;
         int current = 0;
@@ -63,8 +64,14 @@ public class QueryResultReader implements Iterator<List<Tuple>> {
     // Method to increment a reader by a set
     private void setCurrentRowNext() {
         if (this.hasNext()) {
+
+            // Iterator starts before the list, thus when at this "before" position, move to the first index to read
+            if (this.currentStartIndex == -1) {
+                this.currentStartIndex = 0;
+            }
+
             // Check if incrementing the end index will go over the number of tuples
-            if (this.currentEndIndex + this.numberOfRowsPerSet > (this.tupleCount-1)) {
+            else if (this.currentEndIndex + this.numberOfRowsPerSet > (this.tupleCount-1)) {
                 // If so, get the difference and set the end index to the tuple count
                 this.lastDifference = (this.currentEndIndex + this.numberOfRowsPerSet) - (this.tupleCount-1);
                 this.currentEndIndex = this.tupleCount-1;
@@ -115,18 +122,34 @@ public class QueryResultReader implements Iterator<List<Tuple>> {
         return returnList;
     }
 
+    /**
+     * Checks if the row set pointer is currently at the first row set (NOT before or after)
+     * @return true if the row set pointer is currently at the first row set, false otherwise
+     */
+    public boolean isAtStart() {
+        return this.currentStartIndex == 0;
+    }
+
     @Override
     public boolean hasNext() {
-        return this.currentEndIndex != this.tupleCount;
+        return (this.currentEndIndex != (this.tupleCount-1));
     }
 
+    /**
+     * Sets the row set pointer to the next row set (if applicable) and returns said row set.
+     * @return The next row set (a {@link List} of {@link Tuple}s). If call is made while the row set pointer is at the last row set, the row set pointer will stay as is and thus, the last row set will be returned
+     */
     @Override
     public List<Tuple> next() {
-        List<Tuple> nextSet = this.getCurrentSet();
         this.setCurrentRowNext();
-        return nextSet;
+        return this.getCurrentSet();
     }
 
+    /**
+     * Gets the previous row set and sets the row set pointer back to said previous row set (if applicable).
+     * @return The previous row set (a {@link List} of {@link Tuple}s). If call is made while current result set pointer is before the first set, the pointer will be
+     * incremented to the first set and thus, the first set will be returned. Any calls while the pointer is at the first row set will return the first row set
+     */
     public List<Tuple> previous() {
         this.setCurrentRowPrevious();
         return this.getCurrentSet();
